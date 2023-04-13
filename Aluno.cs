ed4a2ba61e66a783eb145a44021bb28756692996
Hugo de Araujo <hugo.saraujo@tutanota.com>
2023-04-13 19:26:27 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matricula
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool status { get; set; }
+        public string Curso { get; set; }
+
+        private int matricula;
+        public int Matricula 
+        {
+            get 
+            { 
+                return matricula; 
+            }
+            set 
+            { 
+                if (value <= 0) 
+                {
+                    Console.WriteLine("Valor da Matrícula é inválido");
+                    Console.WriteLine("O valor precisa ser maior que 0");
+                } 
+                else
+                {
+                    matricula = value;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Matricula;
+
+Aluno aluno = new Aluno();
+aluno.Matricula = -0909081092;
+Console.WriteLine(aluno.Matricula);
